e5f9ec2ac3e3132edc63b1fa57cc2e8d3bd97f99
jamalbalya <jamal.balya@gmail.com>
2023-04-15 22:59:39 +0700

    changes pop-up message into voice message (v1.1.0)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 import subprocess
+import pyttsx3
+
 
 class BatteryMonitorUI:
     def __init__(self, master):
         self.master = master
-        master.title("Battery Monitor")
+        master.title("Battery Monitoring Jemz Be v1.1.0")
+
+        # Initialize text-to-speech engine
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 170)
 
         # Create labels and entry fields
         low_label = tk.Label(master, text="Low Battery Level:")
@@ -56,17 +62,18 @@ class BatteryMonitorUI:
                 battery_level = int(battery_info.split()[5].replace('%;', ''))
             except (ValueError, subprocess.CalledProcessError):
                 # Handle errors gracefully
-                tk.messagebox.showerror("Error", "Failed to retrieve battery level")
+                self.engine.say("Failed to retrieve battery level")
+                self.engine.runAndWait()
                 return
 
             if battery_level <= low_battery_level:
                 # Show low battery message
-                tk.messagebox.showwarning("Low Battery Warning",
-                                          f"The battery level is at {battery_level}%. Please connect the charger.")
+                self.engine.say(f"The battery level is at {battery_level}%. Please connect the charger.")
+                self.engine.runAndWait()
             elif battery_level >= high_battery_level:
                 # Show high battery message
-                tk.messagebox.showinfo("High Battery Alert",
-                                       f"The battery level is at {battery_level}%. Please unplug the charger.")
+                self.engine.say(f"The battery level is at {battery_level}%. Please unplug the charger.")
+                self.engine.runAndWait()
 
             # Wait for 1 minute before checking again
             self.master.after(60000, monitor_battery_level)
